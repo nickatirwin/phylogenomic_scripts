32b1e0cb9ed5653bd0c38f8f7d3d5e1130aa9b58
nickatirwin <nickatirwin@gmail.com>
2025-02-09 23:42:32 +0100

    Updated protein_prediction.py to include clustering

--- scripts/protein_prediction.py
+++ scripts/protein_prediction.py
@@ -18,7 +18,7 @@ gc = sys.argv[3]
 threads = sys.argv[4]
 
 # run miniprot
-subprocess.call('miniprot -Iut16 -t ' + threads + ' --trans ' + genome + ' ' + proteins + ' -T ' + gc + ' > '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot',shell=True)
+subprocess.call('miniprot -Iut16 -t ' + threads + ' --trans ' + genome + ' ' + proteins + ' -T ' + gc + ' > '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot',shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 
 # parse output
 mini = open(genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot','r').readlines()
@@ -26,7 +26,16 @@ out = open(genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta','w'
 n = 0
 p = 1
 while n < len(mini):
-    out.write('>'+genome.split('.')[0]+'.'+mini[n].split('\t')[0]+'_'+str(p)+'\n'+mini[n+1].strip().split('\t')[1]+'\n')
+    out.write('>'+genome.split('.')[0]+'.seq_'+str(p)+'\n'+mini[n+1].strip().split('\t')[1]+'\n')
     n += 2
     p += 1
 out.close()
+
+# cluster the resulting sequences at 100% identity
+subprocess.call('cd-hit -i '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta'+' -o ' + genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta.100c' + ' -c 1.0 -T '+threads,shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+
+# remove temp files
+subprocess.call('mv '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta.100c '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta',shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+subprocess.call('rm '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta.100c.clstr')
+
+
