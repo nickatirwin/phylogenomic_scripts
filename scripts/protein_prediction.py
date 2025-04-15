de3f8e9d9c773a0a5b1b2feb8ad66754cfe783fa
nickatirwin <nickatirwin@gmail.com>
2025-04-15 18:47:33 +0200

    Updated protein prediction

--- scripts/protein_prediction.py
+++ scripts/protein_prediction.py
@@ -18,24 +18,27 @@ gc = sys.argv[3]
 threads = sys.argv[4]
 
 # run miniprot
-subprocess.call('miniprot -Iut16 -t ' + threads + ' --trans ' + genome + ' ' + proteins + ' -T ' + gc + ' > '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot',shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+subprocess.call('miniprot -Iut16 -t ' + threads + ' --trans ' + genome + ' ' + proteins + ' -T ' + gc + ' > '+genome.split('/')[-1].split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot',shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
 
 # parse output
-mini = open(genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot','r').readlines()
-out = open(genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta','w')
-n = 0
-p = 1
-while n < len(mini):
-    out.write('>'+genome.split('.')[0]+'.seq_'+str(p)+'\n'+mini[n+1].strip().split('\t')[1]+'\n')
-    n += 2
-    p += 1
-out.close()
+mini = open(genome.split('/')[-1].split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot','r').readlines()
 
-# cluster the resulting sequences at 100% identity
-subprocess.call('cd-hit -i '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta'+' -o ' + genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta.100c' + ' -c 1.0 -T '+threads,shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+if len(mini) > 0:
+    out = open(genome.split('/')[-1].split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta','w')
+    n = 0
+    p = 1
+    while n < len(mini):
+        if '##STA' in mini[n+1]:
+            out.write('>'+genome.split('/')[-1].split('.')[0]+'.seq_'+str(p)+'\n'+mini[n+1].strip().split('\t')[1]+'\n')
+            p += 1
+        n += 2
+    out.close()
 
-# remove temp files
-subprocess.call('mv '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta.100c '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta',shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
-subprocess.call('rm '+genome.split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta.100c.clstr',shell=True)
+    # cluster the resulting sequences at 100% identity
+    subprocess.call('cd-hit -i '+genome.split('/')[-1].split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta'+' -o ' + genome.split('/')[-1].split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta.100c' + ' -c 1.0 -T '+threads,shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+
+    # remove temp files
+    subprocess.call('mv '+genome.split('/')[-1].split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta.100c '+genome.split('/')[-1].split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta',shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+    subprocess.call('rm '+genome.split('/')[-1].split('.')[0]+'.'+proteins.split('.')[0]+'.miniprot.fasta.100c.clstr',shell=True)
 
 
